ceaade26723deca813e5feb91594f09591dc4594
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-05-31 17:32:38 +0600

    Upload Products and adding or querying products for home page

--- store/models.py
+++ store/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 # Customers
 class Customer(models.Model):
     first_name = models.CharField(max_length=60)
