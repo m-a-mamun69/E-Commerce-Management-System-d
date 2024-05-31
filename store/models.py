3a8b0d7590cd091a9b815d3067cd9aedf4351746
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-05-31 18:07:07 +0600

    add Two Field in Product Model

--- store/models.py
+++ store/models.py
@@ -31,6 +31,10 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=200, default='', blank=True)
     image = models.ImageField(upload_to='uploads/product/')
+    # Add Sales Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    
 
     def __str__(self):
         return self.name
