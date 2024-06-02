701eed4bad4bfbdb519cf217a7aaa30b4ee38a33
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-02 18:55:55 +0600

    Adding Login Template And Login Functionality Stuff

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
@@ -10,4 +13,13 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    pass
+
 
