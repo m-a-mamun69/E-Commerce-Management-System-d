d0401181e0418b20b6e631bfa94e6c753b9c4f47
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-02 17:35:28 +0600

    Add Three Tamplate Files For the website And Separate Them

--- store/views.py
+++ store/views.py
@@ -5,3 +5,9 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
+
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
