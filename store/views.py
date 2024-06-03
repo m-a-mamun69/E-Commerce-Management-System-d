660376642765a2735f7c0590410215954c73812b
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-03 19:19:02 +0600

    Adding Regiater File And Functionality Stuff And Add forms.py File Also

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 
 # Create your views here.
@@ -35,6 +39,27 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out....thanks For Stopping By...."))
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #log in User
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You Have Register Successfully!!!..Welcome"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Whoops!.. There was a Problem Registering. Plase try Again..."))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
+
 
 
 
