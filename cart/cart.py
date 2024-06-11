54988e97b8282ccbbe2a98334692d471dc0c42b1
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-11 20:42:29 +0600

    End-up The 'Update Shopping Cart' Sessions with ValueError

--- cart/cart.py
+++ cart/cart.py
@@ -43,3 +43,15 @@ class Cart():
     def get_quants(self):
         quantities = self.cart
         return quantities
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        # Get Cart
+        ourcart = self.cart
+        # Update Dictionary/ Cart
+        ourcart[product_id] = product_qty
+        self.session.modified = True
+        thing = self.cart
+        return thing
--- cart/views.py
+++ cart/views.py
@@ -41,4 +41,16 @@ def cart_delete(request):
 
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+
+        # get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty',0))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty':product_qty})
+        return response
+        # return redirect('cart_summary')
+    
