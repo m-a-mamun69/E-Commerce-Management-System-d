aa319dc2cb414596a136508b779ea56f8e5b20d2
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-14 16:47:08 +0600

    - Add update_user File
    - Update User Profile Functionality Stuff

--- store/views.py
+++ store/views.py
@@ -4,11 +4,29 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm
 from django import forms
 
 
 # Create your views here.
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, ("User Has Been Updated!..."))
+            return redirect('home')
+        return render(request, 'update_user.html', {"user_form":user_form})
+    else:
+        messages.success(request, ("You must Be Logged in to Access That Page!..."))
+        return redirect('home')
+
+
 
 def category_summary(request):
     categories = Category.objects.all()
