9569f834328e07e51f9133a89b7b777c977b78d0
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-15 12:11:43 +0600

    Create New Model And Register For User Profile Page

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username","first_name", "last_name", "email"]
+    inlines = [ProfileInLine]
+
+# UnRegister The Old way
+admin.site.unregister(User)
+
+# Re-Register the new way
+admin.site.register(User, UserAdmin)
+
