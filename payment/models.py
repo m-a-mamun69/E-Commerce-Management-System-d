8b9699bba04c13ba4a370f79f949ea133f54c0c1
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-21 17:43:48 +0600

    Solved Shipping Model Problem

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -23,6 +24,14 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
 
+# Create a User Shipping Address by default when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+# Automate the Shipping thing
+post_save.connect(create_shipping, sender=User)
 
 
 # Create Order Model
