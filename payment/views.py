10e399a2f8b812fb9884364893cd1a3aee8fc12c
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-22 11:56:39 +0600

    Adding billing_info File And Billing Info Functionality Stuff Part_one

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
 from payment.forms import ShippingForm
 from payment.models import ShippingAddress
+from django.contrib import messages
 # Create your views here.
+
+def billing_info(request):
+    if request.POST:
+        #Get the Cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # Check to see if user is logged In
+        if request.user.is_authenticated:
+            return render(request, 'payment/billing_info.html', {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_info":request.POST})
+        else:
+            # Not Logged In
+            pass
+        shipping_form = request.POST
+        return render(request, 'payment/billing_info.html', {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+    else:
+        messages.success(request, "Access Denied!")
+        return redirect('home')
+
 
 def checkout(request):
     #Get the Cart
