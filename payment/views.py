d1a06e84a552d7dbf8594f16fc1f2732032cd5a1
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-06-26 17:31:08 +0600

    Clear Shopping Cart Model

--- payment/views.py
+++ payment/views.py
@@ -4,7 +4,7 @@ from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from django.contrib import messages
-from store.models import Product
+from store.models import Product, Profile
 import datetime
 # Create your views here.
 
@@ -135,6 +135,11 @@ def process_order(request):
                 if key == "session_key":
                     # Delete the key
                     del request.session[key]
+
+            # Delete Cart from Database (Old_Cart Field)
+            current_user = Profile.objects.filter(user__id=request.user.id)
+            # Delete Shopping cart in Database (Old_Cart Field)
+            current_user.update(old_cart="")
 
 
             messages.success(request, "Order Placed!")
