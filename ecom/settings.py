2705163e486dc9af34394c6e1fc6e712723a21ca
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-06 18:55:05 +0600

    Some Changes for Deployment Error in settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = 'django-insecure-cm2pm^1(d_cw@l@e)0a1)q()9(i5e!vo2m#je^$zvkza45l6e_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-ecommerce-production-bb09.up.railway.app', '*']
-CSRF_TRUSTED_ORIGINS = ['https://django-ecommerce-production-bb09.up.railway.app']
+ALLOWED_HOSTS = [ 'localhost', '127.0.0.1','django-ecommerce-production-bb09.up.railway.app' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://django-ecommerce-production-bb09.up.railway.app' ]
 
 
 # Application definition
