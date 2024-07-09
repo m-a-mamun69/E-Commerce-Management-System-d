a4911863b1c8d145a32863a82fb0618031a5c307
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-09 18:13:43 +0600

    Little Change in Setteing File.

--- ecom/settings.py
+++ ecom/settings.py
@@ -30,7 +30,7 @@ DB_PASSWORD_YO = os.environ['DB_PASSWORD_YO']
 SECRET_KEY = 'django-insecure-cm2pm^1(d_cw@l@e)0a1)q()9(i5e!vo2m#je^$zvkza45l6e_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ 'localhost', '127.0.0.1','django-ecommerce-production-bb09.up.railway.app' ]
 CSRF_TRUSTED_ORIGINS = [ 'https://django-ecommerce-production-bb09.up.railway.app' ]
