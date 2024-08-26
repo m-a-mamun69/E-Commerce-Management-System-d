05e81932738b9a364a837412103bd01fd4d9ac4e
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-08-26 17:29:29 +0600

    CleanUp 'Add Postgres Cloud Database' Commit And Ready For Re-build.

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,16 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load our environmental variables
-load_dotenv()
+# load_dotenv()
 
 # password DB
-DB_PASSWORD_YO = os.environ['DB_PASSWORD_YO']
+# DB_PASSWORD_YO = os.environ['DB_PASSWORD_YO']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': DB_PASSWORD_YO,
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '21566',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'railway',
+        # 'USER': 'postgres',
+        # 'PASSWORD': DB_PASSWORD_YO,
+        # 'HOST': 'roundhouse.proxy.rlwy.net',
+        # 'PORT': '21566',
 
 
         
